c00a65df2ea06c76c6f2f225d27eea3d3f279d17
Ahmed Salim Alhajri <48984962+hajricod@users.noreply.github.com>
2022-04-04 10:36:04 +0400

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,21 +30,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $notifications    = Notification::orderBy('status', 'desc')->orderBy('created_at', 'desc')->limit(5)->get();
-        $notifCounter     = Notification::where('status', '1')->get();
-        $headerLinks      = HeaderLink::where('status', 1)->orderBy('sort')->get();
-        $headerSublinks   = HeaderSublink::where('status', 1)->orderBy('sort')->get();
-        $footerCategories = FooterCategory::where('status', 1)->orderBy('sort')->get();
-        $footerLinks      = FooterLink::where('status', 1)->orderBy('sort')->get();
+        // $notifications    = Notification::orderBy('status', 'desc')->orderBy('created_at', 'desc')->limit(5)->get();
+        // $notifCounter     = Notification::where('status', '1')->get();
+        // $headerLinks      = HeaderLink::where('status', 1)->orderBy('sort')->get();
+        // $headerSublinks   = HeaderSublink::where('status', 1)->orderBy('sort')->get();
+        // $footerCategories = FooterCategory::where('status', 1)->orderBy('sort')->get();
+        // $footerLinks      = FooterLink::where('status', 1)->orderBy('sort')->get();
 
         Paginator::useBootstrap();
-        View::share([
-            'notifications'    => $notifications, 
-            'notifCounter'     => $notifCounter,
-            'headerLinks'      => $headerLinks,
-            'headerSublinks'   => $headerSublinks,
-            'footerCategories' => $footerCategories,
-            'footerLinks'      => $footerLinks
-        ]);
+        // View::share([
+        //     'notifications'    => $notifications, 
+        //     'notifCounter'     => $notifCounter,
+        //     'headerLinks'      => $headerLinks,
+        //     'headerSublinks'   => $headerSublinks,
+        //     'footerCategories' => $footerCategories,
+        //     'footerLinks'      => $footerLinks
+        // ]);
     }
 }
